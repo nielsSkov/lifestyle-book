1d96e3a47938ed9d695dd8556fd97f541d226ad1
nielsSkov <v.niels.skov@gmail.com>
2026-08-05 03:37:33 +0200

    Clarify daily timeline tiles

--- daily_categories.py
+++ daily_categories.py
@@ -8,18 +8,27 @@ class DailyCategory:
     group: str
     colour: str
     selected_colour: str
+    tile_colour: str
     icon: str
     active: bool = True
 
 
 DAILY_CATEGORIES = (
-    DailyCategory("walk", "Walk", "movement", "#78b68b", "#3b5745", "walk"),
-    DailyCategory("run", "Run", "movement", "#dc786f", "#68413f", "run"),
-    DailyCategory("swim", "Swim", "movement", "#61a9c4", "#34505d", "swim"),
-    DailyCategory("dance", "Dance", "movement", "#b77bc9", "#563d60", "dance"),
-    DailyCategory("cycling", "Bike", "movement", "#d0a64f", "#62522d", "cycling"),
-    DailyCategory("low_sugar", "Low Sugar", "food", "#8fb76f", "#465a35", "low-sugar"),
-    DailyCategory("cooked", "Cooked", "food", "#cf865f", "#604137", "cooked"),
+    DailyCategory("walk", "Walk", "movement", "#78b68b", "#3b5745", "#5a8768", "walk"),
+    DailyCategory("run", "Run", "movement", "#dc786f", "#68413f", "#a25d57", "run"),
+    DailyCategory("swim", "Swim", "movement", "#61a9c4", "#34505d", "#4b7d91", "swim"),
+    DailyCategory("dance", "Dance", "movement", "#b77bc9", "#563d60", "#875c95", "dance"),
+    DailyCategory("cycling", "Bike", "movement", "#d0a64f", "#62522d", "#997c3e", "cycling"),
+    DailyCategory(
+        "low_sugar",
+        "Low Sugar",
+        "food",
+        "#8fb76f",
+        "#465a35",
+        "#6b8952",
+        "low-sugar",
+    ),
+    DailyCategory("cooked", "Cooked", "food", "#cf865f", "#604137", "#98644b", "cooked"),
 )
 
 
