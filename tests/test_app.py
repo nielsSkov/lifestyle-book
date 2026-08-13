f024581825c8b727d7f61aa7ab40def731080a6c
nielsSkov <v.niels.skov@gmail.com>
2026-08-13 15:00:42 +0200

    Allow backup preview deselection

--- tests/test_app.py
+++ tests/test_app.py
@@ -180,6 +180,9 @@ def test_weight_plan_page_lists_automatic_backups(client):
     assert b"13 Aug 2026, 12:58:10 CEST" in response.data
     assert backup.name.encode() in response.data
     assert b"1 of 30" in response.data
+    assert b'type="checkbox" value="plan-auto-' in response.data
+    assert b"if (!input.checked) {" in response.data
+    assert b"if (other !== input) other.checked = false;" in response.data
     assert response.data.index(b'data-delete-backup="') < response.data.index(
         b'data-restore-backup="'
     )
